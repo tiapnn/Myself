1e16ad3ff2e2a333efc490e2e4effffb35b3ccd1
Mattia Penna <tiapnn@yahoo.it>
2021-05-08 06:44:28 +0200

    feat: add resist & clearMind function

--- MattiaPenna.js
+++ MattiaPenna.js
@@ -1,6 +1,29 @@
+import Courage;
+
 class MattiaPenna {
+
   constructor() {
     this.name = Mattia;
     this.yearOfBirth = 1994;
+    this.nationality = Italian;
   }
+
+
+  function resist (): Courage {
+    if (this.isSad && this.feelDiscomfort) {
+      console.group();
+      console.log("Resist");
+      console.log("You can go through this");
+      console.groupEnd();
+      this.clearMind();
+      return new Courage();
+    }
+  }
+
+  function clearMind (): void {
+    let mind = this.MindBin;
+    mind.clear();
+  }
+
+
 }
